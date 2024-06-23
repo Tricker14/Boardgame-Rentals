2f750418ed8631ea4acc8f63ef3d2e1dbb9b5e60
Tricker14 <trietthetricker@gmail.com>
2024-06-24 01:03:08 +0700

    config cors & restrict method for rest api

---
+++ backend/spring-boot-boardgame-rentals/src/main/java/com/example/spring_boot_boardgame_rentals/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.example.spring_boot_boardgame_rentals.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry cors) {
+        cors.addMapping("/**").allowedMethods("*");
+    }
+}
