40660cf851c800d1e04b2ec66318918047b00fb5
Tricker14 <trietthetricker@gmail.com>
2024-06-30 13:33:36 +0700

    add search category search

--- backend/spring-boot-boardgame-rentals/src/main/java/com/example/spring_boot_boardgame_rentals/repository/BoardGameRepository.java
+++ backend/spring-boot-boardgame-rentals/src/main/java/com/example/spring_boot_boardgame_rentals/repository/BoardGameRepository.java
@@ -1,7 +1,14 @@
 package com.example.spring_boot_boardgame_rentals.repository;
 
 import com.example.spring_boot_boardgame_rentals.entity.BoardGame;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface BoardGameRepository extends JpaRepository<BoardGame, Long> {
+    Page<BoardGame> findByNameContaining(String name, Pageable pageable);
+    Page<BoardGame> findByCategory(String category, Pageable pageable);
 }
